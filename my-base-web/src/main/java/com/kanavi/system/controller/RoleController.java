5cc8e7bd252b960ab05b7847091e583b6c794f12
tanghailan <gdx_lwx@qq.com>
2020-10-17 18:36:18 +0800

    提交文档修改

--- my-base-web/src/main/java/com/kanavi/system/controller/RoleController.java
+++ my-base-web/src/main/java/com/kanavi/system/controller/RoleController.java
@@ -6,6 +6,8 @@ import com.kanavi.response.api.ResponseBean;
 import com.kanavi.system.entity.Role;
 import com.kanavi.system.service.RoleService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,7 +38,11 @@ public class RoleController {
     private RoleService roleService;
 
     @GetMapping
-    @ApiOperation("根据参数查询角色列表")
+    @ApiOperation(value = "根据参数查询角色列表", notes = "分页查询列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "current", value = "当前页"),
+            @ApiImplicitParam(name = "size", value = "页数"),
+    })
     public ResponseBean findRoleList(@RequestParam(required = true,defaultValue = "1")int current,
                                      @RequestParam(required = true,defaultValue = "7")int size){
         List<Role> list = roleService.list();
